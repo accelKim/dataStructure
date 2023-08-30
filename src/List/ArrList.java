c7cc2171cb35eb725b08406d6a355af85a064f20
accelKim <dnjsgh567877@gmail.com>
2023-08-30 15:51:08 +0900

    23.08.30

---
+++ src/List/ArrList.java
@@ -0,0 +1,56 @@
+package List;
+
+import java.util.ArrayList;
+
+public class ArrList<E> {
+  //인스턴스 변수
+  private E a[]; //private E() a;
+  private int size;
+
+  public int size() {
+    return size;
+  }
+
+  //생성자
+  public ArrList() {
+    a = (E[]) new Object[1];
+    size = 0;
+  }
+  //연산 메서드
+  public E get(int k){
+    if(size == 0)
+      throw new IndexOutOfBoundsException();
+    return a[k];
+  }
+
+  public boolean add(E e) {
+    if (a.length == size){
+//      throw new ArrayIndexOutOfBoundsException();
+      resize(a.length * 2);
+    }
+    a[size++] = e;
+    return true;
+  }
+
+  private  void resize(int newSize) {
+    System.out.println("resize가 호출됨" + newSize);
+    E[] t = (E[]) new Object[newSize];
+    for (int i = 0; i < size; i++) {
+      t[i] = a[i];
+    }
+    a = t;
+  }
+
+  public E remove(){ //removeLast
+    if(size == 0)
+      throw new IndexOutOfBoundsException();
+    E tobeDeleted = a[size - 1];
+    a[size-1] = null;
+    size--;
+    if (size > 0 && size == a.length/4) {
+      resize(a.length / 2);
+    }
+    return tobeDeleted;
+  }
+
+}
---
+++ src/List/ArrListTest.java
@@ -0,0 +1,35 @@
+package List;
+
+import java.util.ArrayList;
+
+public class ArrListTest {
+  public static void main(String[] args) {
+    ArrList<Integer> list = new ArrList<>();
+    System.out.println(list.size());
+    list.add(10);
+    System.out.println(list.get(0));
+    list.add(20);
+    System.out.println(list.get(1));
+    list.add(30);
+    list.add(40);
+    list.add(50);
+    System.out.println(list.remove() + "가 삭제됨");
+    System.out.println(list.remove() + "가 삭제됨");
+    System.out.println(list.remove() + "가 삭제됨");
+    System.out.println(list.remove() + "가 삭제됨");
+    System.out.println(list.remove() + "가 삭제됨");
+    System.out.println(list.size());
+
+    ArrayList<Integer> alist = new ArrayList<>();
+    System.out.println(alist.size());
+    alist.add(10);
+    alist.add(20);
+    alist.add(30);
+    alist.add(40);
+    alist.remove(alist.size() - 1);
+    alist.remove(alist.size() - 1);
+    System.out.println(alist.get(0));
+    System.out.println(alist.size());
+
+  }
+}
